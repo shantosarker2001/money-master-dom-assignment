034e335295cca7c64c306bb51b71177132098053
shantosarker2001 <sarkershant0303@gmail.com>
2022-08-22 01:17:56 +0530

    select player

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
-const classes = document.querySelectorAll("#get_classes .btn");
-console.log(classes);
+const buttons = document.querySelectorAll(".get button");
+
+for (let button of buttons) {
+    button.addEventListener('click', function (event) {
+        event.target.setAttribute("disabled", true);
+        const title = event.target.parentNode.childNodes[1].innerText
+        // console.log(title)
+        const container = document.getElementById('list_container')
+        // console.log(container)
+        const newOl = document.createElement('ol')
+        const newLi = document.createElement('li')
+        newLi.innerText = title
+        newOl.appendChild(newLi)
+        container.appendChild(newOl)
+        console.log(newOl)
+    })
+}
